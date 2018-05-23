b210cbe6f42025e8259032bbbe9dcb048a510639
Ayo <31580516+emdele@users.noreply.github.com>
2018-05-23 23:38:50 +0100

    added HelloWorld

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		for(int x = 1; x<10; x++) {
+		
+		System.out.println("hello world there!! = " +x);
+
+	}
+
+}
+}
